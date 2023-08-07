0a7d46fc36043df64a4413643f3c766acfff8185
yorsh.reynoso <yorsh.reynoso@gmail.com>
2023-08-07 12:48:12 -0600

    adding some try catch that were missing on the CRUD, now the crud is complete

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -4,12 +4,18 @@ const User = require('../models/user.model');
 
 
 router.get('/all', async(req, res) => {
-    const users = await User.find();
-    console.log(users);
-    if(users) {
-        res.json(users)
-    } else {
-        res.status(404).send('no Users found');
+    try {
+        const users = await User.find();
+        console.log(users);
+        if(users) {
+            res.json(users)
+        } else {
+            res.status(404).send('no Users found');
+        }
+        
+    } catch (error) {
+        console.log(error);
+        res.status(404).send("There was an error trying to get all users");
     }
 });
 
@@ -53,10 +59,16 @@ router.put('/update/:userId', async(req, res) => {
     const { userId } = req.params;
     const { username, email, firstName, lastName, phoneNumber, city, birthday, gender, password, active} =  req.body; // this is not neccessary, mongoose check the differences.
 
-    const updatedUser = await User.findByIdAndUpdate(userId, req.body, {upsert: false});
+    try {
+        const updatedUser = await User.findByIdAndUpdate(userId, req.body, {upsert: false});
+        console.log(`post: ${updatedUser}, modified`);
+        res.send(`Id ${userId} received correctly`);
+        
+    } catch (error) {
+        console.log(error);
+        res.status(404).send('The user wasnt modified correctly');
+    }
 
-    console.log(`post: ${updatedUser}, modified`);
-    res.send(`Id ${userId} received correctly`);
 });
 
 router.delete('/delete/:userId', async(req, res) => {
